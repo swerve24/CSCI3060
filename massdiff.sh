2079338a1f39a361e3f49fc1601ba5eac7581dc3
Luisa Rojas <r.luisaf@gmail.com>
2016-01-19 16:26:23 -0500

    Edited .sh file

--- massdiff.sh
+++ massdiff.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+echo "Hello Word"
+
+#takes one argument (file_name)
+if [ "$1"  != ""]; then
+	echo "Argument provided: $1"
+else
+	echo "Please enter an argument"
+	#end
+fi
+
+#for each file in the current directory:
+	#diff file and file_name
+	#if they are the same:
+		#echo "file and file_name are the same"
+		echo "File and file_name are the same"
+	#else
+		#append the diff to a log file
