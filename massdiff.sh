cb3b54b0370c567b2810b9c11e18f1bc5084d697
Luisa Rojas <r.luisaf@gmail.com>
2016-01-19 16:59:49 -0500

    Deleted all that didn't work

--- massdiff.sh
+++ massdiff.sh
@@ -6,17 +6,21 @@ echo "CSCI 3060 - Lab 1"
 if [ "$#"  == 1 ]; then
 	echo "Argument provided: $1"
 	FILE_NAME = "$1"
+	LOG_FILE = "lab1_logfile.log"
+	
+	CURRENT_DIR = pwd
 
 	#for each file in the current directory:
-	CURRENT_DIR = pwd
 	#diff file and file_name
 	for FILES in $CURRENT_DIR do
-	#if they are the same:
-	if [ diff $FILE_NAME/$FILES == ""]; then
-		#echo "file and file_name are the same"
-		echo "File and file_name (argument) are the same"
-	else
-		#append the diff to a log file
+		#if they are the same:
+		if #diff function
+			; then
+			#echo "file and file_name are the same"
+			echo "File and file_name (argument) are the same"
+		else
+			#append the diff to a log file
+		fi
 	done
 
 else
