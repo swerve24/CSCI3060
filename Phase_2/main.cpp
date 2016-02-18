d0430ac74a3343c671e60bfad18739f37d54db7f
TTTruong <truyen.truong@uoit.net>
2016-02-18 16:05:05 -0500

    Added more to main.

--- Phase_2/main.cpp
+++ Phase_2/main.cpp
@@ -2,27 +2,137 @@
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <string.h>
 #include "User.cpp"
 
 using namespace std;
 
+void printWelcomeMessage() {
+	cout << "Welcome to Watermelon Banking System" << endl;
+	cout << "Please log in to begin or type in \"help\" for more information." << endl;
+}
+
+void printHelp() {
+	cout << "HELP ME PLS." << endl;
+}
+
 int main (int argc, char *argv[]) {
 
-	User u("Bob", "Active", "Non-student", 00001, 00500.00);
 
-	cout << u.getName() << endl;
-	cout << u.getStatus() << endl;
-	cout << u.getPlan() << endl;
-	cout << u.getAccNum() << endl;
-	cout << u.getBalance() << endl;
 
-	if (argc == 0) { // for command line inputs only
+	printWelcomeMessage();
 
-	} else  if (argc == 0) { // argc should be 1 for correct execution with text inputs
+	if (argc == 1) { // for command line inputs only
+
+		string command;
+		string mode;
+		bool logged = false;
+
+		while(true) {
+
+			cout << "Enter a command." << endl;
+			cin >> command;
+
+			if (command.compare("login") == 0) {
+				if (!logged) {
+
+					cout << "Enter mode in which you wish to log in as: ";
+					cin >> mode;
+
+					if (mode.compare("admin") == 0) {
+						mode = "admin";
+						logged = true;
+						cout << endl << "You are currently logged in as an administrator." << endl;
+
+					} else if (mode.compare("standard") == 0) {
+
+						cout << "Enter account holder's name: ";
+
+					} else {
+						cerr << "ERROR: Invalid account mode." << endl;
+					}
+
+				} else {
+					cerr << "ERROR: There is a session running. Please log out and try again." << endl;
+				}
+			} else if (command.compare("logout") == 0) {
+				if (logged) {
+					logged = false;
+				} else {
+					cerr << "ERROR: You are not currently logged into an account." << endl;
+				}
+			} else if (command.compare("create") == 0) {
+				if (logged) {
+
+				} else {
+					cerr << "ERROR: Must be logged in before invoking any other commands." << endl;
+				}
+			} else if (command.compare("delete") == 0) {
+				if (logged) {
+
+				} else {
+					cerr << "ERROR: Must be logged in before invoking any other commands." << endl;
+				}
+			} else if (command.compare("disable") == 0) {
+				if (logged) {
+
+				} else {
+					cerr << "ERROR: Must be logged in before invoking any other commands." << endl;
+				}
+			} else if (command.compare("enable") == 0) {
+				if (logged) {
+
+				} else {
+					cerr << "ERROR: Must be logged in before invoking any other commands." << endl;
+				}
+			} else if (command.compare("deposit") == 0) {
+				if (logged) {
+
+				} else {
+					cerr << "ERROR: Must be logged in before invoking any other commands." << endl;
+				}
+			} else if (command.compare("withdrawal") == 0) {
+				if (logged) {
+
+				} else {
+					cerr << "ERROR: Must be logged in before invoking any other commands." << endl;
+				}
+			} else if (command.compare("transfer") == 0) {
+				if (logged) {
+
+				} else {
+					cerr << "ERROR: Must be logged in before invoking any other commands." << endl;
+				}
+			} else if (command.compare("paybill") == 0) {
+				if (logged) {
+
+				} else {
+					cerr << "ERROR: Must be logged in before invoking any other commands." << endl;
+				}
+			} else if (command.compare("changeplan") == 0) {
+				if (logged) {
+
+				} else {
+					cerr << "ERROR: Must be logged in before invoking any other commands." << endl;
+				}
+			} else if (command.compare("help") == 0) {
+				printHelp();
+			} else if (command.compare("quit") == 0) {
+				if (!logged) {
+					return 1;
+				} else {
+					cerr << "ERROR: You may not quit while logged in." << endl;
+				}
+			} else {
+				cerr << "ERROR: Invalid command." << endl;
+			}
+		}
+
+	} else  if (argc == 2) { // argc should be 1 for correct execution with text inputs
     	// argv[0] would be the first argument - <filename>
 
  	} else {
-
+ 		cerr << "ERROR: Too many arguments." << endl;
+ 		return -1;
 	}
 }
