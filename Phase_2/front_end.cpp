38d463d0f7f088e233dd863f5b7b0d1abccfdcc5
Luisa Rojas <r.luisaf@gmail.com>
2016-02-18 13:20:18 -0500

    Added some code and updated readme file

---
+++ Phase_2/front_end.cpp
@@ -0,0 +1,23 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class User {
+	public:
+		User();
+		User(string name, int acc_num, float balance, string status, string plan);
+};
+
+int main (int argc, char *argv[]) {
+
+  if (argc == 0) { // for command line inputs only
+
+  } else  if (argc == 0) { // argc should be 1 for correct execution with text inputs
+    // argv[0] would be the first argument - <filename>
+
+  } else {
+
+  }
+}
